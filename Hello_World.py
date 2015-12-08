3633e182b0e556f75a497d65ad30787a9716aede
shoemakercha <shoemakercha@urbandaleschools.com>
2015-12-08 09:27:49 -0600

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,3 +1,11 @@
 from _future_ import print_function
 # The above line imports Python 3.0 printing - NEVER FORGET THE ABOVE LINE OF CODE!
 print("Hello World")
+print("2 + 3")
+print(2 + 3)
+print(Hello World)
+print("Your new score is", 1030 + 10)
+print("Your new score is", "1030 + 10")
+print("Your new score is," 1030 + 10)
+print("Your new score is,", 1030 + 10)
+print("I want to print a double quote " for some reason.")
