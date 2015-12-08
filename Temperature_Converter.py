0ed52ebecd490e11cd6660c7f57e5d1c982029fb
shoemakercha <shoemakercha@urbandaleschools.com>
2015-12-08 12:54:12 -0600

    Update Temperature_Converter.py

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,3 +1,4 @@
+from __future__ import print_function 
 temperature_f = int(raw_input("Enter Temperature in Fahrenheit:"))
 Celsius = (temperature_f - 32) * (5 / 9)
-print("Temperature:", C)
+print("Temperature:", "C")
