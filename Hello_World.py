b9d1108724c639008a8d074a6a668926d104c799
shoemakercha <shoemakercha@urbandaleschools.com>
2015-12-08 10:18:40 -0600

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,4 @@
-from _future_ import print_function
+from __future__ import print_function
 # The above line imports Python 3.0 printing - NEVER FORGET THE ABOVE LINE OF CODE!
 print("Hello World")
 print("2 + 3")
@@ -9,3 +9,35 @@ print("Your new score is", "1030 + 10")
 print("Your new score is," 1030 + 10)
 print("Your new score is,", 1030 + 10)
 print("I want to print a double quote " for some reason.")
+print("I want to print a double quote \" for some reason.")
+print("The file in stored in C:\new folder")
+print("The file is stored in C:\\new folder")
+from __future__ import print_function
+print("CSE")
+print("Rocks!")
+from __future__ import print_function
+print("CSE", end=" ")
+print("Rocks!")
+from __future__ import print_function
+i = 0
+print(i, end=" ")
+i = 1
+print(i, end=" ")
+# This is a comment, it begins with a # sign
+# and the computer will ignore it.
+print("This is not a comment, the computer will")
+print("run this and print it out.")
+print("A # sign between quotes is not a comment.")
+# print("This is a comment, even if it is computer code.")
+print("Hi") # This is an end-of-line comment
+print("Hi")
+'''
+This is
+a
+multi
+line
+comment. Nothing
+Will run in between these quotes.
+print("There")
+'''
+print("Done")
