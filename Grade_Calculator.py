2ad782367e0174f64d882faf5b09e52f5d398a74
shoemakercha <shoemakercha@urbandaleschools.com>
2015-12-08 12:31:10 -0600

    Update Grade_Calculator.py

--- Grade_Calculator.py
+++ Grade_Calculator.py
@@ -1,1 +1,6 @@
-
+from __future__ import print_function 
+Semester_Grade = int(raw_input("Enter Semester Grade:", %"))
+Final_Grade = int(raw_input("Enter Final Exam Grade:", %"))
+Final_Exam_Weight = int(raw_input("Enter Final Exam Weight:", %"))
+Final_Grade = (Semester_Grade * 0.8) + (Final_Grade * Final_Exam_Weight)
+print("Your Final Grade:")
